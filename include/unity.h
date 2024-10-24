7e141565224ca2312aeba24838823893215f6e60
LiberiBg <matheo.ngn@gmail.com>
2024-10-24 10:48:14 +0200

    update: move features.h into include

--- include/unity.h
+++ include/unity.h
@@ -19,7 +19,7 @@ extern "C"
 {
 #endif
 
-#include "unity_internals.h"
+#include "../include/unity_internals.h"
 
 /*-------------------------------------------------------
  * Test Setup / Teardown
